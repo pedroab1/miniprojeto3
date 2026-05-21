d4b59d6a03cd0284105725bba0cfd20982acca81
Pedro A Batista <pedroazevedobatista@gmail.com>
2026-05-20 22:17:53 -0300

    Update main.py

--- meu_projeto_api/client/main.py
+++ meu_projeto_api/client/main.py
@@ -8,3 +8,17 @@ def testar_conexao():
         print("GET / :", response.json())
     except requests.exceptions.ConnectionError:
         print("Erro de conexao.")
+
+def enviar_texto_para_ia(texto_usuario):
+    payload = {"texto": texto_usuario}
+    try:
+        response = requests.post(f"{API_URL}/analisar", json=payload)
+        if response.status_code == 200:
+            print("POST /analisar :", response.json())
+    except requests.exceptions.ConnectionError:
+        print("Erro de conexao.")
+
+if __name__ == "__main__":
+    testar_conexao()
+    enviar_texto_para_ia("O projeto esta excelente")
+    enviar_texto_para_ia("Deu erro no sistema")
